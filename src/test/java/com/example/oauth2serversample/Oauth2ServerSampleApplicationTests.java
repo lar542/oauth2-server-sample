0f797d4b1764927a64698604fc9b545ec551ac52
gugu <lar542@naver.com>
2019-08-19 17:04:37 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/oauth2serversample/Oauth2ServerSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.oauth2serversample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2ServerSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
